ed1201b1e0ec8ed36281fcfe91f8380c5e48203b
Rahatul07 <rifarhad08@gmail.com>
2023-04-10 19:36:01 +0600

    design improved

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className=" lg:flex justify-between my-container  items-center">
         <div>
           <h1 className=" pt-7 pb-10 text-4xl lg:text-7xl font-bold ">
-            YOUR <span className="text-blue-400">DREAM JOB</span> HAS ALREADY
+            YOUR <span className="text-purple-600">DREAM JOB</span> HAS ALREADY
             HAPPENED{" "}
           </h1>
           <p className="pb-5  ">
@@ -27,36 +27,36 @@ const Home = () => {
         </div>
       </div>
       <div className="text-center mb-10">
-        <h1 className="pb-2 text-center text-4xl font-bold">
+        <h1 className="pb-2 text-center text-4xl font-bold ">
           Job Category List
         </h1>
         <p>
           Explore thousands of job opportunities with all the information you
           need. Its your future
         </p>
-        <div className="mb-5  md:grid grid-cols-2 lg:grid-cols-4 mx-10 my-10">
-          <div className="bg-sky-50 w-60 rounded-xl text-left p-7 ml-5 py-10">
+        <div className="mb-5  md:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4  my-10 container mx-auto ">
+          <div className="bg-purple-50 w-60 rounded-xl text-left p-7 mx-auto  py-10  ">
             <img className="pb-3 text-sky-100" src={img1} alt="" />
-            <h1 className="text-lg font-bold">Account & Finance</h1>
+            <h1 className="text-lg font-bold ">Account & Finance</h1>
             <p>
               <small>300 Jobs Available</small>
             </p>
           </div>
-          <div className="bg-sky-50 w-60 rounded-xl text-left p-7 ml-5 py-10">
+          <div className="bg-purple-50 w-60 rounded-xl text-left p-7 ml-5 py-10">
             <img className="pb-3 text-sky-100" src={img2} alt="" />
             <h1 className="text-lg font-bold">Creative Design</h1>
             <p>
               <small>100+ Jobs Available</small>
             </p>
           </div>
-          <div className="bg-sky-50 w-60 rounded-xl text-left p-7 ml-5 py-10">
+          <div className="bg-purple-50 w-60 rounded-xl text-left p-7 ml-5 py-10">
             <img className="pb-3 text-sky-100" src={img3} alt="" />
             <h1 className="text-lg font-bold">Marketing & Sales</h1>
             <p>
               <small>150 Jobs Available</small>
             </p>
           </div>
-          <div className="bg-sky-50 w-60 rounded-xl text-left p-7 ml-5 py-10">
+          <div className="bg-purple-50 w-60 rounded-xl text-left p-7 ml-5 py-10">
             <img className="pb-3 text-sky-100" src={img4} alt="" />
             <h1 className="text-lg font-bold">Engineering Job</h1>
             <p>
