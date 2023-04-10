424281f89f697c32e7e749200e364b5c601ffc82
Rahatul07 <rifarhad08@gmail.com>
2023-04-10 16:23:55 +0600

    nested router setup done

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Statistics from "./components/Statistics/Statistics";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import Blogs from "./components/Blogs/Blogs";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/",
-    element: <Header />,
+    // loader: serviceAndCartData,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/statistics", element: <Statistics /> },
+      { path: "/appliedJobs", element: <AppliedJobs /> },
+      { path: "/blogs", element: <Blogs /> },
+    ],
   },
 ]);
 
